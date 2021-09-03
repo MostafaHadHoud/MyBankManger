f5e1ad0b907e111729a2954dc016f2c6a2610fb6
MostafaHadHoud <90060292+MostafaHadHoud@users.noreply.github.com>
2021-09-04 04:27:47 +0900

    Update Client.h

--- Bank Account Project/Client.h
+++ Bank Account Project/Client.h
@@ -1,11 +1,38 @@
 #ifndef CLIENT_H
 #define CLIENT_H
+#include "Person.h"
 
-class Client
+class Client : public Person
 {
 	public:
 		Client();
+		
+		
+		
+		void setBalance(double b){
+			if (b>=1500){
+				blance = b;
+	
+			}else{
+				cout<<"the minimum blance is 1500, please enter again"<<endl;
+			}
+		}
+		
+		double getBalance(){
+				return blance = b;
+		}
+		
+		void setCurrency(string C){
+			currency = C;
+		}
+		
+		string getCurrency(){
+			return currency;
+		}
+		
 	protected:
+		double balance;
+		string currency;
 };
 
 #endif
