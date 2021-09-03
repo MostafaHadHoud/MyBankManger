aba6e952d6eae258ec91088012d27e704c520a31
MA7MOUDSAMIER <90063023+MA7MOUDSAMIER@users.noreply.github.com>
2021-09-03 22:18:29 +0200

    Update Employee.h

--- Bank Account Project/Employee.h
+++ Bank Account Project/Employee.h
@@ -2,11 +2,42 @@
 #define EMPLOYEE_H
 #include "Person.h"
 
-class Employee  : public Person
+class Employee : public Person
 {
 	public:
+		
 		Employee();
+		
+		void setPassword(string pinCode ){
+			if(pinCode.size()>=8 && pinCode.size()<20){
+				password=pinCode ;
+			}else{
+				cout<<"the paswword must be between 8 and 20 numbers"<< endl;
+			}
+		}
+		
+		void setSalary(double s){
+			if (s>=5000){
+				salary = s;
+	
+			}else{
+				salary = 5000 ;
+			}
+		}
+		
+		string getPassword (){
+			return password ;
+		}
+		
+		double getSalary (){
+			return salary ;
+		}
+		
 	protected:
+		
+		string password ;
+		double salary ;
+		
 };
 
 #endif
