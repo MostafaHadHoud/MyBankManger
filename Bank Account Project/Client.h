2f6096d4bf9beb02c6c9a927fe021ef1d3d69a01
ahmedadelvovo <90062562+ahmedadelvovo@users.noreply.github.com>
2021-09-04 18:14:21 +0200

    Update Client.h

--- Bank Account Project/Client.h
+++ Bank Account Project/Client.h
@@ -1,38 +1,65 @@
 #ifndef CLIENT_H
 #define CLIENT_H
+#include "Terms.h"
 #include "Person.h"
+using namespace std ;
 
 class Client : public Person
 {
-	public:
-		Client();
+	private:
+		string pin_code ;
+		double balance ;
+		string Currency ;
 		
-		
-		
-		void setBalance(double b){
-			if (b>=1500){
-				blance = b;
-	
-			}else{
-				cout<<"the minimum blance is 1500, please enter again"<<endl;
+	public :
+		// constructor
+		Client (){
+			pin_code = " " ;
+			balance = 0 ;
+			Currency = " " ;
+		}
+			// Setter
+			void setPinCode (string n){
+			Terms t1 ;
+			if( (t1.isDigits(n) == 1) && (n.length() == 4) ){
+				pin_code = n ;
+			}
+			else {
+				cout << "pinCode must be digits chars and the size = 4" ;
+			}			
+			
+		}						
+		void setBalance (double b){
+			if ( b>=1500){
+				balance = b ;	
+			}
+			else {
+				cout << " Min balance is 1500 " << endl ;
 			}
 		}
-		
-		double getBalance(){
-				return blance = b;
+		void setCurrency (string c){
+			Currency = c ;
+		}
+			// Getter			
+		string getPinCode (){
+		return pin_code ;
+		}		
+		double getBalance (){
+		return balance ;
+		}
+				
+		string getCurrency (){
+		return Currency ;
 		}
 		
-		void setCurrency(string C){
-			currency = C;
+			// Display Function
+						
+			void display (){
+			Person :: display () ;
+			cout << "PinCode is = " << this->getPinCode() <<endl;
+			cout << "Balance is = " << this->getBalance() <<endl;
+			cout << "Currency is = "<< this->getCurrency() <<endl;	
 		}
-		
-		string getCurrency(){
-			return currency;
-		}
-		
-	protected:
-		double balance;
-		string currency;
 };
 
 #endif
