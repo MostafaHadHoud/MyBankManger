797ad7fa6dcf36a66f139e4e6c9bfb6e871f7f71
ahmedadelvovo <90062562+ahmedadelvovo@users.noreply.github.com>
2021-09-04 18:13:54 +0200

    Update Person.h

--- Bank Account Project/Person.h
+++ Bank Account Project/Person.h
@@ -1,62 +1,68 @@
 #ifndef PERSON_H
 #define PERSON_H
+#include "Terms.h"
+using namespace std ;
 
 class Person
 {
-	public:
-		Person();
-		
-		void setName(string n){
-			if(n.size()>=5 && n.size()<20){
-				name=n;
-			}else{
-				cout<<"this name is too long write a shorter name"<<endl;
-			}
-		}
-		
-		string getName(){
-				return name;
-		}
-		
-		void setNationalId(string I){
-			if(I.size()==14){
-				national_id=I;
-			}else{
-				cout<<"Unvalid ID"<<endl;	
-			}
-		}
-		
-		string getNationalId(){
-				return national_id;
-		}
-		
-		
-		void setPin_code(string PC){
-			if(PC.size()==4){
-				pin_code=PC;
-			}else{
-				cout<<"Unvalid pin code"<<endl;	
-			}
-			
-		}
-		
-		string getPin_code(){
-				return pin_code;
-		}
-		
-		void setId(int Id){
-			this.id=Id;
-		}
-		
-		
-		int setId(){
-			return id;
-		}
 	protected:
-		string name;
-		string national_id;
-		string pin_code;
-		int id;
+		string name ;
+		string national_id ;
+		int id ;	
+				
+	public:
+	// constructor	
+	Person (){
+		name = " ";
+		national_id = " ";
+		id = 0 ;
+	}	
+	//setters	
+	void setName (string n){
+		Terms t1 ;
+			if( t1.isAlphabetic(n) == 0&& t1.isLenght(n,5,20) == 1){
+				name = n ;
+			}
+			else {
+				cout << "the name must be alphabetic chars and min size 5 and max size 20" ;
+			}		
+		}
+				
+	void setNationalId (string n){
+		Terms t1 ;
+			if( (t1.isDigits(n) == 1) && (n.length() == 14) ){
+				national_id = n ;
+			}
+			else {
+				cout << " national id must be digits chars and the size = 14" ;
+			}					
+		}		
+		void setId (int i){
+		id = i ;
+		}		
+	
+				
+		// Getters		
+			string getName (){
+			return name ;
+		}		
+		string getNationalId (){
+		return national_id ;
+		}
+				
+		int getId (){
+		return id ;
+		}	
+		
+		// Display Function
+		void display (){
+			cout << "name is = " << this->getName() <<endl;
+			cout << "National Id is = " << this->getNationalId() << endl;
+			cout << "Id is = " << this->getId() << endl;	
+		}		
+		
+		
+		
 };
 
 #endif
