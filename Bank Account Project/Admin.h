707b5f3024dbe653eecfbef5505d67180e6306f4
ahmedadelvovo <90062562+ahmedadelvovo@users.noreply.github.com>
2021-09-12 00:09:56 +0200

    Update Admin.h

--- Bank Account Project/Admin.h
+++ Bank Account Project/Admin.h
@@ -1,1 +1,28 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include "Employee.h"
 
+using namespace std ;
+
+class Admin : public Employee
+{
+	public:
+		
+		
+		
+		//Operator Overloading to save object to file
+		friend ostream& operator<<(ostream& os, Admin& s)
+	{
+		// write out individual members of s with an end of line between each one
+		os <<"Name          : " <<s.name << '\n';
+		os <<"National Id   : " <<s.national_id << '\n';
+		os <<"Id            : " <<s.id << '\n';
+		os <<"Password      : " <<s.password << '\n';
+		os <<"Salary        : " << s.salary<< '\n' ;
+		os  << "---------------"<< '\n' ;
+		return os;
+		
+	}
+};
+
+#endif
