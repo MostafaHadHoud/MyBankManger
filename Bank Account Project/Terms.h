d8a58e43e616dde9cdbba22845dc26ee701bc167
ahmedadelvovo <90062562+ahmedadelvovo@users.noreply.github.com>
2021-09-12 00:09:16 +0200

    Update Terms.h

--- Bank Account Project/Terms.h
+++ Bank Account Project/Terms.h
@@ -3,54 +3,46 @@
 using namespace std ;
 class Terms{
 	public:
+//------------------------------------------------------------------------------------------
 		// 	string must be alphabetic chars
-	int isAlphabetic(string s){
-		int v = 0 ;
+static bool isAlphabetic(string s){
+		bool v = true ;
 	for (int i = 0; i < s.length(); i++){
 	if (s[i] >= 'a' && s[i] <= 'z'  || s[i] >= 'A' && s[i] <= 'Z'  ){
 	}
-	else {
-		v++ ;
-		}
+	else 
+		v = false ;
 	}
 					return v ;
 	}
-
+//------------------------------------------------------------------------------------------
 		// 	min size  and max size of string
-	bool isLenght(string s ,int a , int b ){
-	if (s.length() >=a && s.length() <=b ){
+static bool isLenght(string s ,int min_size , int max_size ){
+	if (s.length() >=min_size && s.length() <=max_size )
 		return true;	
-	}
-	else {
-		return false ;
+	else 
+		return false ;	
 	}	
-	}	
+//------------------------------------------------------------------------------------------
 		// 	string must be digits chars
-	bool isDigits (string s){
+static bool isDigits (string s){
 		for (int i = 0; i < s.length(); i++){
-	if (s[i] >= '0' && s[i] <= '9' ){
+	if (s[i] >= '0' && s[i] <= '9' )
 					return true;
-	}
-		else{
-			return  false;
-		}		
-	} 
-		
+		else
+			return  false;		
+	} 		
 }
+//------------------------------------------------------------------------------------------
 		// 	string mustn’t contain white space
-	bool Withoutspace (string s){
+static bool Withoutspace (string s){
 		for (int i = 0; i < s.length(); i++){
-	if (s[i] != ' ' ){
+	if (s[i] != ' ' )
 					return true;
-	}
-		else{
-			return  false;
-		}		
-	} 
-		
-}
-		
-	
+		else
+			return  false;		
+	} 		
+}	
 };
 
 #endif
