79e612b96df62880349ab257aa7c21081d0e9380
ahmedadelvovo <90062562+ahmedadelvovo@users.noreply.github.com>
2021-09-12 00:07:49 +0200

    Update Employee.h

--- Bank Account Project/Employee.h
+++ Bank Account Project/Employee.h
@@ -6,10 +6,9 @@ using namespace std ;
 
 class Employee : public Person 
 {
-	private:
+	protected:
 	string password ;
 	double salary ;
-	
 	
 	public:
 		// constructor 
@@ -17,45 +16,49 @@ class Employee : public Person
 			password = " ";
 			salary = 0 ;
 		}
-		
 		// Setters		
-		void setPassword (string p){
-			Terms t1 ;
-					if( t1.Withoutspace(p) == 1 && t1.isLenght(p,8,20) == 1){
-				password = p ;
-			}
-			else {
+	void setPassword (string p){
+					if( Terms::Withoutspace(p) && Terms::isLenght(p,8,20))
+				password = p ;	
+			else 
 				cout << "Error :password must not contain white space and the sizebetween 8 and 20 .\n\n" ;
 			}
-		}
-		
-		void setSalary (double b){
-			if ( b>=5000){
+	void setSalary (double b){
+			if ( b>=5000)
 				salary = b ;	
-			}
 			else {
 				salary = 5000 ;
 				cout << " Error :Min Salary is 5000 .\n\n" << endl ;
 			}
-	}
-			
+		}			
 			// Getter					
-		string getPassword (){
+	string getPassword (){
 			return password ;
-		}
-		
-		
-		double getSalary () {
+		}		
+	double getSalary () {
 			return salary ;
 		}
-			// Display			
-		
-			void display () {
+			// Display					
+	void display () {
 			Person :: display () ;
-			cout << "password    = " << this->getPassword() <<endl;
-			cout << "Salary      = " << this->getSalary() <<endl;
+			cout << "password      = " << this->getPassword() <<endl;
+			cout << "Salary        = " << this->getSalary() <<endl;
 		}	
 		
+			//Operator Overloading to save object to file
+		friend ostream& operator<<(ostream& os, Employee& s)
+	{
+		os <<"Name          : " <<s.name << '\n';
+		os <<"National Id   : " <<s.national_id << '\n';
+		os <<"Id            : " <<s.id << '\n';
+		os <<"Password      : " <<s.password << '\n';
+		os <<"Salary        : " << s.salary<< '\n' ;
+		os  << "---------------"<< '\n' ;
+		return os;
+		
+	}
+	
+				
 };
 
 #endif
